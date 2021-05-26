d8d883141dd2b98cd5370c7aa3739c7c3a0da3c8
VHNhat <nhatvh.work@gmail.com>
2021-05-26 20:22:09 +0700

    Initial commit

---
+++ gira/src/main/java/cybersoft/javabackend/java11/gira/FiraApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java11.gira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiraApplication.class, args);
+	}
+
+}
