f8be7175f97333ae0aae0390c278fc233455f1c0
VHNhat <nhatvh.work@gmail.com>
2021-06-14 17:18:04 +0700

    dev user module
    fix bug addNewUser()

--- gira/src/main/java/cybersoft/javabackend/java11/gira/user/repository/UserRepository.java
+++ gira/src/main/java/cybersoft/javabackend/java11/gira/user/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package cybersoft.javabackend.java11.gira.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import cybersoft.javabackend.java11.gira.user.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	int countByUsername(String username);
